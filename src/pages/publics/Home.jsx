89b0a096befa5e798e5360da07b291394b1a08d4
tanphatran <152074205+tanphatran@users.noreply.github.com>
2025-04-22 12:44:19 +0700

    #fix: video home

--- src/pages/publics/Home.jsx
+++ src/pages/publics/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NatureVid from "../../assets/video/main.mov";
+import NatureVid from "../../assets/video/main.mp4";
 import Hero from "../../components/Hero/Hero";
 import Rooms from '../../components/Rooms/Rooms';
 
